72a82050f157bfc8e2e30c126238030854db9e68
worldserpentx <142296908+worldserpentx@users.noreply.github.com>
2024-04-03 15:50:14 +0800

    X

---
+++ user/shop.php
@@ -0,0 +1,43 @@
+<?php
+
+require ("../head.html");
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShopPay</title>
+    
+</head>
+<body>
+    <header>
+        <h1>Shop Pay</h1>
+        <a href="#" class="cart"><i class="fas fa-shopping-cart"></i></a>
+    </header>
+
+    <div class="container">
+        <div class="item">
+            <img src="../images/lenovo-1.jpg" alt="Product Image">
+            <h2>Lenovo ThinkBook 13s</h2>
+            <p>₱18,999.00</p>
+            <button class="add-to-cart"><i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="item">
+            <img src="../images/chair-2.jpg" alt="Product Image">
+            <h2>Office/gaming Chair</h2>
+            <p>₱21,999.00</p>
+            <button class="add-to-cart"><i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="item">
+            <img src="../images/gpu-3.jpg" alt="Product Image">
+            <h2>RTX 2060</h2>
+            <p>₱19,899.00</p>
+            <button class="add-to-cart"><i class="fas fa-cart-plus"></i></button>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
